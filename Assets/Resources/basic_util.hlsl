b8a5f72f5b2d57d5975719ed9283a7d754f9922e
Donat Deva Brzoska <donat.brzoska@gmail.com>
2023-06-19 22:29:52 +0200

    extract function for similar in range checks

--- Assets/Resources/basic_util.hlsl
+++ Assets/Resources/basic_util.hlsl
@@ -6,3 +6,11 @@ bool is_relevant_thread(uint2 id, uint2 calculation_size)
 float unzero(float f) {
     return f + 0.000001;;
 }
+
+bool pixel_in_array_range(int2 pixel, uint2 target_size)
+{
+    return pixel.x >= 0
+        && pixel.x < (int) target_size.x
+        && pixel.y >= 0
+        && pixel.y < (int) target_size.y;
+}
