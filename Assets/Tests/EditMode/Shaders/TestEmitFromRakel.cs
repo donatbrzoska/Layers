25144dd0547d00cb3b83f2ebab26fdbd511baff3
Donat Deva Brzoska <donat.brzoska@gmail.com>
2023-06-30 11:25:07 +0200

    reimplement emit from rakel tests

--- Assets/Tests/EditMode/Shaders/TestEmitFromRakel.cs
+++ Assets/Tests/EditMode/Shaders/TestEmitFromRakel.cs
@@ -1,280 +1,284 @@
-﻿//using System;
-//using NUnit.Framework;
-//using UnityEngine;
+﻿using System;
+using NUnit.Framework;
+using UnityEngine;
 
-//public class TestEmitFromRakel
-//{
-//    private const float EMIT_DISTANCE_MAX = 0.1f;
+public class TestEmitFromRakel
+{
+    Canvas_ Canvas;
+    float RakelLength = 4;
+    float RakelWidth = 2;
+    Rakel Rakel;
 
-//    Canvas_ Canvas;
-//    float RakelLength = 4;
-//    float RakelWidth = 2;
-//    Rakel Rakel;
+    int MAX_LAYERS = 20;
 
-//    ColorFiller ColorFiller;
+    ColorFiller ColorFiller;
 
-//    ComputeBuffer RakelEmittedPaint;
+    PaintGrid RakelEmittedPaint;
 
-//    public float Sum(float[] values)
-//    {
-//        float res = 0;
-//        foreach (float e in values) {
-//            res += e;
-//        }
-//        return res;
-//    }
+    public float Sum(float[] values)
+    {
+        float res = 0;
+        foreach (float e in values)
+        {
+            res += e;
+        }
+        return res;
+    }
 
-//    [SetUp]
-//    public void Setup()
-//    {
-//        Canvas = new Canvas_(15, 10, new Vector3(0, 0, 0), 1, 0, 0);
+    [SetUp]
+    public void Setup()
+    {
+        Canvas = new Canvas_(15, 10, MAX_LAYERS, new Vector3(0, 0, 0), 1, 0, 0);
 
-//        Rakel = new Rakel(RakelLength, RakelWidth, 1, 0.5f, 0);
+        Rakel = new Rakel(RakelLength, RakelWidth, 1, MAX_LAYERS, 0.5f, 0);
 
-//        ColorFiller = new FlatColorFiller(Color_.CadmiumGreen, ColorSpace.RGB);
+        ColorFiller = new FlatColorFiller(Color_.CadmiumGreen, ColorSpace.RGB);
 
-//        new FileLogger_().OnEnable();
-//    }
+        new FileLogger_().OnEnable();
+    }
 
-//    [TearDown]
-//    public void Teardown()
-//    {
-//        RakelEmittedPaint.Dispose();
-//        Rakel.Dispose();
+    [TearDown]
+    public void Teardown()
+    {
+        RakelEmittedPaint.Dispose();
+        Rakel.Dispose();
+        Canvas.Dispose();
 
-//        new FileLogger_().OnDisable();
-//    }
+        new FileLogger_().OnDisable();
+    }
 
-//    [Test]
-//    public void Volume_Unrotated_Untilted()
-//    {
-//        // Arrange
-//        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(1)));
-//        Rakel.UpdateState(
-//            new Vector3(-3, -0.5f, -0.5f * EMIT_DISTANCE_MAX), 0, // choose z for max paint emission
-//            0,
-//            0);
-//        ShaderRegion rakelEmitSR = new ShaderRegion(
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerRight),
-//            1 // not really needed for polygon clipping
-//        );
+    [Test]
+    public void EmitOneLayer_IncompleteSurfacesTouch_Unrotated_Untilted()
+    {
+        int INCOMPLETE_SURFACES_TOUCH_VOLUME = 1;
+        int APPLIED_LAYERS = 1;
 
-//        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas, false);
+        // Arrange
+        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(INCOMPLETE_SURFACES_TOUCH_VOLUME)));
+        Rakel.UpdateState(
+            new Vector3(-5, -0.5f, APPLIED_LAYERS * -Paint.VOLUME_THICKNESS), 0, 0,
+            0,
+            0);
+        ShaderRegion rakelEmitSR = new ShaderRegion(
+            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerRight)
+        );
 
-//        // Act
-//        RakelEmittedPaint = Rakel.EmitPaint(
-//            rakelEmitSR,
-//            Canvas,
-//            rakelMappedInfo,
-//            false);
-
-
-//        // Assert
-//        Paint[] rakelEmittedPaintData = new Paint[rakelEmitSR.PixelCount];
-//        RakelEmittedPaint.GetData(rakelEmittedPaintData);
-//        float[] rakelEmittedVolumes = new float[rakelEmitSR.PixelCount];
-//        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
-//        {
-//            rakelEmittedVolumes[i] = rakelEmittedPaintData[i].Volume;
-//        }
-
-//        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSC.Size.y, false);
-
-//        float Q = 0.25f * Paint.UNIT; // quarter
-//        float H = 0.5f * Paint.UNIT; // half
-//        float F = Paint.UNIT; // full
-//        AssertUtil.AssertFloatsAreEqual(
-//            new float[] { // remember: these arrays are upside down compared to the actual pixels
-//                0, 0, 0, 0, 0,
-//                0, Q, H, Q, 0,
-//                0, H, F, H, 0,
-//                0, H, F, H, 0,
-//                0, H, F, H, 0,
-//                0, Q, H, Q, 0,
-//                0, 0, 0, 0, 0,
-//            },
-//            rakelEmittedVolumes);
-
-//        Assert.AreEqual(
-//            RakelLength * RakelWidth * Paint.UNIT,
-//            Sum(rakelEmittedVolumes));
-//    }
-
-//    [Test]
-//    public void Volume_Rotated30_Untilted()
-//    {
-//        // Arrange
-//        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(1)));
-//        Rakel.UpdateState(
-//            new Vector3(-3, -0.5f, -0.5f * EMIT_DISTANCE_MAX), 0, // choose z for max paint emission
-//            30,
-//            0);
-//        ShaderRegion rakelEmitSR = new ShaderRegion(
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerRight),
-//            1 // not really needed for polygon clipping
-//        );
-
-//        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas, false);
-
-//        // Act
-//        RakelEmittedPaint = Rakel.EmitPaint(
-//            rakelEmitSR,
-//            Canvas,
-//            rakelMappedInfo,
-//            false);
+        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas);
+        Rakel.Reservoir.Duplicate();
+        Rakel.CalculateRakelMappedInfo_Part2(Canvas, rakelMappedInfo, rakelEmitSR, 0);
 
 
-//        // Assert
-//        Paint[] rakelEmittedPaintData = new Paint[rakelEmitSR.PixelCount];
-//        RakelEmittedPaint.GetData(rakelEmittedPaintData);
-//        float[] rakelEmittedVolumes = new float[rakelEmitSR.PixelCount];
-//        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
-//        {
-//            rakelEmittedVolumes[i] = rakelEmittedPaintData[i].Volume;
-//        }
-
-//        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSC.Size.y, false);
-
-//        AssertUtil.AssertFloatsAreEqual(
-//            new float[] { // remember: these arrays are upside down compared to the actual pixels
-//                 0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.008f,  0.054f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.173f,  0.801f,  0.655f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.116f,  0.961f,  0.997f,  0.235f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.500f,  1.000f,  0.801f,  0.008f,  0.000f,
-//                 0.000f,  0.000f,  0.039f,  0.881f,  0.655f,  0.054f,  0.000f,
-//                 0.000f,  0.000f,  0.000f,  0.062f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//            },
-//            rakelEmittedVolumes);
-
-//        AssertUtil.AssertFloatsEqual(
-//            RakelLength * RakelWidth * Paint.UNIT,
-//            Sum(rakelEmittedVolumes));
-//    }
-
-//    [Test]
-//    public void Volume_Unrotated_Tilted60()
-//    {
-//        // Arrange
-//        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(1)));
-//        Rakel.UpdateState(
-//            new Vector3(
-//                -4,
-//                -0.5f,
-//                -0.5f * EMIT_DISTANCE_MAX), 0,
-//            0,
-//            60);
-
-//        ShaderRegion rakelEmitSR = new ShaderRegion(
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerRight),
-//            1 // not really needed for polygon clipping
-//        );
-
-//        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas, false);
-
-//        // Act
-//        RakelEmittedPaint = Rakel.EmitPaint(
-//            rakelEmitSR,
-//            Canvas,
-//            rakelMappedInfo,
-//            false);
+        // Act
+        RakelEmittedPaint = Rakel.EmitPaint(
+            rakelEmitSR,
+            Canvas,
+            rakelMappedInfo);
 
 
-//        // Assert
-//        Paint[] rakelEmittedPaintData = new Paint[rakelEmitSR.PixelCount];
-//        RakelEmittedPaint.GetData(rakelEmittedPaintData);
-//        float[] rakelEmittedVolumes = new float[rakelEmitSR.PixelCount];
-//        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
-//        {
-//            rakelEmittedVolumes[i] = rakelEmittedPaintData[i].Volume;
-//        }
+        // Assert
+        RakelEmittedPaint.Readback();
+        float[] rakelEmittedVolumes = new float[RakelEmittedPaint.InfoData.Length];
+        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
+        {
+            rakelEmittedVolumes[i] = RakelEmittedPaint.InfoData[i].Volume;
+        }
 
-//        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSC.Size.y, false);
+        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSR.Size.y, false);
 
-//        AssertUtil.AssertFloatsAreEqual(
-//            new float[] { // remember: these arrays are upside down compared to the actual pixels
-//                0.000f,  0.000f,  0.000f,  0.000f,
-//                0.000f,  0.012f,  0.000f,  0.000f,
-//                0.000f,  0.024f,  0.000f,  0.000f,
-//                0.000f,  0.024f,  0.000f,  0.000f,
-//                0.000f,  0.024f,  0.000f,  0.000f,
-//                0.000f,  0.012f,  0.000f,  0.000f,
-//                0.000f,  0.000f,  0.000f,  0.000f,
-//            },
-//            rakelEmittedVolumes);
+        float Q = 0.25f * Paint.UNIT;
+        float H = 0.5f * Paint.UNIT;
+        float F = Paint.UNIT;
+        AssertUtil.AssertFloatsAreEqual(
+            new float[] { // remember: these arrays are upside down compared to the actual pixels
+                0, 0, 0,
+                0, F, 0,
+                0, F, 0,
+                0, F, 0,
+                0, 0, 0,
+            },
+            rakelEmittedVolumes);
 
-//        AssertUtil.AssertFloatsEqual(
-//            0.0958637297f * Paint.UNIT,
-//            Sum(rakelEmittedVolumes));
-//    }
+        Assert.AreEqual(
+            3 * Paint.UNIT,
+            Sum(rakelEmittedVolumes));
+    }
 
-//    [Test]
-//    public void Volume_Rotated30_Tilted60()
-//    {
-//        // Arrange
-//        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(1)));
-//        Rakel.UpdateState(
-//            new Vector3(
-//                -3,
-//                -0.5f,
-//                -0.1f * EMIT_DISTANCE_MAX), 0,
-//            30,
-//            10);
+    [Test]
+    public void EmitOneLayer_CompleteSurfacesTouch_Unrotated_Untilted()
+    {
+        int COMPLETE_SURFACES_TOUCH_VOLUME = 4;
+        int LAYERS = 1;
 
-//        ShaderRegion rakelEmitSR = new ShaderRegion(
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
-//            Canvas.MapToPixelInRange(Rakel.Info.LowerRight),
-//            1 // not really needed for polygon clipping
-//        );
+        // Arrange
+        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(COMPLETE_SURFACES_TOUCH_VOLUME)));
+        Rakel.UpdateState(
+            new Vector3(-5, -0.5f, LAYERS * -Paint.VOLUME_THICKNESS), 0, 0,
+            0,
+            0);
+        ShaderRegion rakelEmitSR = new ShaderRegion(
+            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerRight)
+        );
 
-//        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas, false);
-
-//        // Act
-//        RakelEmittedPaint = Rakel.EmitPaint(
-//            rakelEmitSR,
-//            Canvas,
-//            rakelMappedInfo,
-//            false);
+        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas);
+        Rakel.Reservoir.Duplicate();
+        Rakel.CalculateRakelMappedInfo_Part2(Canvas, rakelMappedInfo, rakelEmitSR, 0);
 
 
-//        // Assert
-//        Paint[] rakelEmittedPaintData = new Paint[rakelEmitSR.PixelCount];
-//        RakelEmittedPaint.GetData(rakelEmittedPaintData);
-//        float[] rakelEmittedVolumes = new float[rakelEmitSR.PixelCount];
-//        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
-//        {
-//            rakelEmittedVolumes[i] = rakelEmittedPaintData[i].Volume;
-//        }
+        // Act
+        RakelEmittedPaint = Rakel.EmitPaint(
+            rakelEmitSR,
+            Canvas,
+            rakelMappedInfo);
 
-//        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSC.Size.y, false);
 
-//        AssertUtil.AssertFloatsAreEqual(
-//            new float[] { // remember: these arrays are upside down compared to the actual pixels
-//                 0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.093f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.069f,  0.042f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.108f,  0.000f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.016f,  0.403f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.000f,  0.018f,  0.000f,  0.000f,  0.000f,
-//                 0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,  0.000f,
-//            },
-//            rakelEmittedVolumes);
+        // Assert
+        RakelEmittedPaint.Readback();
+        float[] rakelEmittedVolumes = new float[RakelEmittedPaint.InfoData.Length];
+        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
+        {
+            rakelEmittedVolumes[i] = RakelEmittedPaint.InfoData[i].Volume;
+        }
 
-//        AssertUtil.AssertFloatsEqual(
-//            0.749224544f,
-//            Sum(rakelEmittedVolumes));
-//    }
-//}
+        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSR.Size.y, false);
+
+        float Q = 0.25f * Paint.UNIT;
+        float H = 0.5f * Paint.UNIT;
+        float F = Paint.UNIT;
+        AssertUtil.AssertFloatsAreEqual(
+            new float[] { // remember: these arrays are upside down compared to the actual pixels
+                Q, H, Q,
+                H, F, H,
+                H, F, H,
+                H, F, H,
+                Q, H, Q,
+            },
+            rakelEmittedVolumes);
+
+        Assert.AreEqual(
+            RakelLength * RakelWidth * Paint.UNIT,
+            Sum(rakelEmittedVolumes));
+    }
+
+    [Test]
+    public void EmitOneLayer_CompleteSurfacesTouch_Rotated30_Untilted()
+    {
+        int BASICALLY_COMPLETE_SURFACES_TOUCH_VOLUME = 20;
+        int LAYERS = 1;
+
+        // Arrange
+        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(BASICALLY_COMPLETE_SURFACES_TOUCH_VOLUME)));
+        Rakel.UpdateState(
+            new Vector3(-5, -0.5f, LAYERS * -Paint.VOLUME_THICKNESS), 0, 0,
+            30,
+            0);
+        ShaderRegion rakelEmitSR = new ShaderRegion(
+            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerRight)
+        );
+
+        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas);
+        Rakel.Reservoir.Duplicate();
+        Rakel.CalculateRakelMappedInfo_Part2(Canvas, rakelMappedInfo, rakelEmitSR, 0);
+
+
+        // Act
+        RakelEmittedPaint = Rakel.EmitPaint(
+            rakelEmitSR,
+            Canvas,
+            rakelMappedInfo);
+
+
+        // Assert
+        RakelEmittedPaint.Readback();
+        float[] rakelEmittedVolumes = new float[RakelEmittedPaint.InfoData.Length];
+        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
+        {
+            rakelEmittedVolumes[i] = RakelEmittedPaint.InfoData[i].Volume;
+        }
+
+        LogUtil.Log(rakelEmittedVolumes, rakelEmitSR.Size.y, false);
+
+        AssertUtil.AssertFloatsAreEqual(
+            new float[] { // remember: these arrays are upside down compared to the actual pixels
+                0.000f,  0.000f,  0.054f,  0.000f,  0.000f,
+                0.173f,  0.801f,  0.655f,  0.000f,  0.000f,
+                0.116f,  0.961f,  0.997f,  0.235f,  0.000f,
+                0.000f,  0.500f,  1.000f,  0.801f,  0.000f,
+                0.000f,  0.000f,  0.881f,  0.655f,  0.054f,
+                0.000f,  0.000f,  0.062f,  0.000f,  0.000f,
+            },
+            rakelEmittedVolumes);
+
+        AssertUtil.AssertFloatsEqual(
+            RakelLength * RakelWidth * Paint.UNIT,
+            Sum(rakelEmittedVolumes),
+            0.1f); // lower precision, because surfaces_touch is really hard to complete with a rotated rakel
+    }
+
+    [Test]
+    public void EmitTwoLayers_CompleteSurfacesTouch_Unrotated_Untilted()
+    {
+        int COMPLETE_SURFACES_TOUCH_VOLUME = 8;
+        int LAYERS = 2;
+
+        // Arrange
+        Rakel.Fill(new ReservoirFiller(ColorFiller, new FlatVolumeFiller(COMPLETE_SURFACES_TOUCH_VOLUME)));
+        Rakel.UpdateState(
+            new Vector3(-5, -0.5f, LAYERS * -Paint.VOLUME_THICKNESS), 0, 0,
+            0,
+            0);
+        ShaderRegion rakelEmitSR = new ShaderRegion(
+            Canvas.MapToPixelInRange(Rakel.Info.UpperLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.UpperRight),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerLeft),
+            Canvas.MapToPixelInRange(Rakel.Info.LowerRight)
+        );
+
+        ComputeBuffer rakelMappedInfo = Rakel.CalculateRakelMappedInfo(rakelEmitSR, Canvas);
+        Rakel.Reservoir.Duplicate();
+        Rakel.CalculateRakelMappedInfo_Part2(Canvas, rakelMappedInfo, rakelEmitSR, 0);
+
+
+        // Act
+        RakelEmittedPaint = Rakel.EmitPaint(
+            rakelEmitSR,
+            Canvas,
+            rakelMappedInfo);
+
+
+        // Assert
+        RakelEmittedPaint.Readback();
+        float[] rakelEmittedVolumes = new float[RakelEmittedPaint.InfoData.Length];
+        for (int i = 0; i < rakelEmittedVolumes.Length; i++)
+        {
+            rakelEmittedVolumes[i] = RakelEmittedPaint.InfoData[i].Volume;
+        }
+
+        //LogUtil.Log(rakelEmittedVolumes, rakelEmitSR.Size.y, false);
+
+        float Q = 0.25f * LAYERS * Paint.UNIT;
+        float H = 0.5f * LAYERS * Paint.UNIT;
+        float F = LAYERS * Paint.UNIT;
+        AssertUtil.AssertFloatsAreEqual(
+            new float[] { // remember: these arrays are upside down compared to the actual pixels
+                Q, H, Q,
+                H, F, H,
+                H, F, H,
+                H, F, H,
+                Q, H, Q,
+            },
+            rakelEmittedVolumes);
+
+        Assert.AreEqual(
+            RakelLength * RakelWidth * LAYERS * Paint.UNIT,
+            Sum(rakelEmittedVolumes));
+    }
+}
