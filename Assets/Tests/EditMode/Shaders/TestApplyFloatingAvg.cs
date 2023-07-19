a2f8b31f1255d3ee2939ba90a2d324f6eee68b36
Donat Deva Brzoska <donat.brzoska@gmail.com>
2023-07-19 17:32:03 +0200

    bring back floating avg ringbuffer implementation

---
+++ Assets/Tests/EditMode/Shaders/TestApplyFloatingAvg.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class TestApplyFloatingAvg
+{
+    ComputeBuffer Volume;
+    float[] VolumeData;
+    ComputeBuffer AvgRingbuffer;
+    float[] AvgRingbufferData;
+    bool StrokeBegin;
+
+    private void Execute()
+    {
+        Volume = new ComputeBuffer(1, sizeof(float));
+        Volume.SetData(VolumeData);
+        AvgRingbuffer = new ComputeBuffer(AvgRingbufferData.Length, sizeof(float));
+        AvgRingbuffer.SetData(AvgRingbufferData);
+
+        new ComputeShaderTask(
+            "RakelState/ApplyFloatingAvg",
+            new ShaderRegion(Vector2Int.zero, Vector2Int.zero, Vector2Int.zero, Vector2Int.zero),
+            new List<CSAttribute>()
+            {
+                    new CSComputeBuffer("ReducedVolumeSource", Volume),
+
+                    new CSComputeBuffer("AvgRingbuffer", AvgRingbuffer),
+                    new CSInt("StrokeBegin", StrokeBegin ? 1 : 0)
+            },
+            true
+        ).Run();
+
+        Volume.GetData(VolumeData);
+        AvgRingbuffer.GetData(AvgRingbufferData);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Volume.Dispose();
+        AvgRingbuffer.Dispose();
+    }
+
+    [Test]
+    public void init()
+    {
+        int RINGBUF_SIZE = 4;
+
+        // Arrange
+        VolumeData = new float[] { 2 };
+        AvgRingbufferData = new float[] { 0, 0, RINGBUF_SIZE, 0, 0, 0, 0 };
+        StrokeBegin = true;
+
+        // Act
+        Execute();
+
+        // Assert
+        Assert.AreEqual(
+            new float[] { 2 },
+            VolumeData);
+
+        Assert.AreEqual(
+            new float[] { 2, 0, RINGBUF_SIZE, 2, 2, 2, 2 },
+            AvgRingbufferData);
+    }
+
+    [Test]
+    public void new_value()
+    {
+        int RINGBUF_SIZE = 4;
+
+        // Arrange
+        VolumeData = new float[] { 4 };
+        AvgRingbufferData = new float[] { 2, 0, RINGBUF_SIZE, 2, 2, 2, 2 };
+        StrokeBegin = false;
+
+        // Act
+        Execute();
+
+        // Assert
+        Assert.AreEqual(
+            new float[] { 2.5f },
+            VolumeData);
+
+        Assert.AreEqual(
+            new float[] { 2.5f, 1, RINGBUF_SIZE, 4, 2, 2, 2 },
+            AvgRingbufferData);
+    }
+
+    [Test]
+    public void new_value_wrap_around_pointer()
+    {
+        int RINGBUF_SIZE = 4;
+
+        // Arrange
+        VolumeData = new float[] { 4 };
+        AvgRingbufferData = new float[] { 2, 3, RINGBUF_SIZE, 2, 2, 2, 2 };
+        StrokeBegin = false;
+
+        // Act
+        Execute();
+
+        // Assert
+        Assert.AreEqual(
+            new float[] { 2.5f },
+            VolumeData);
+
+        Assert.AreEqual(
+            new float[] { 2.5f, 0, RINGBUF_SIZE, 2, 2, 2, 4 },
+            AvgRingbufferData);
+    }
+}
