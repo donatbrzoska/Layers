593e7c723f0ddc6b5d8a64ed8d10242d4efc3803
Donat Deva Brzoska <donat.brzoska@gmail.com>
2023-08-28 02:16:59 +0200

    improve cache performance

--- Assets/Scripts/Reservoir/IndexUtil.cs
+++ Assets/Scripts/Reservoir/IndexUtil.cs
@@ -11,8 +11,8 @@ public class IndexUtil
     public static int XYZ(int x, int y, int z, Vector3Int dim)
     {
         // return z + y * dim.z + x * dim.y * dim.z; // stack by stack, column-major
-        // return z + x * dim.z + y * dim.x * dim.z; // stack by stack, row-major
+        return z + x * dim.z + y * dim.x * dim.z; // stack by stack, row-major
         // return z * dim.y * dim.x + x * dim.y + y; // layer by layer, column-major
-        return z * dim.y * dim.x + y * dim.x + x; // layer by layer, row-major
+        //return z * dim.y * dim.x + y * dim.x + x; // layer by layer, row-major
     }
 }
